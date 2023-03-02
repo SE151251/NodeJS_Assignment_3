e920bac58c99f190e99c4e2bbf0aa0bbf089f7c2
Tien Dat <tiendatthsp@gmail.com>
2023-03-02 16:27:00 +0700

    css login page

--- config/auth.js
+++ config/auth.js
@@ -1,5 +1,3 @@
-var JwtStrategy = require('passport-jwt').Strategy;
-var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
 module.exports = {
